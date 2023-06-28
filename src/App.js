2668d40b688746b474688e56d5a63e2c862c8836
Sujung Ahn <annedec05@naver.com>
2023-06-28 10:15:36 +0900

    add homepage

--- src/App.js
+++ src/App.js
@@ -11,7 +11,7 @@ import Header from "./ui/Header";
 
 function App() {
   return (
-    <BrowserRouter>
+    <BrowserRouter basename={process.env.PUBLIC_URL}>
       <Header />
       <Route exact path="/" component={MainPage} />
       <Route path="/about" component={AboutPage} />
